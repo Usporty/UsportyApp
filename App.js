e25badd296d3a3b1fb127efe534914583a260afa
Eric BATABA <eric.bataba@yahoo.fr>
2018-11-05 00:04:36 +0100

    Premier commit - USporty
    Co-Authored-By: Eric BATABA <eric.bataba@yahoo.fr>

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Usporty </Text>
+        <Text>Go pour un dev de folie !! </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
